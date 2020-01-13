9f34d0b9bc1e9202463769e3c6f65f8334bd2f74
Zheng Li <zhengli@zhengs-air.home>
2020-01-13 16:39:30 +0100

    add structured files

--- src/main.js
+++ src/main.js
@@ -1,19 +1,1 @@
-import hey from './test';
-import { whatIsYourName } from './test';
 import './styles/main.scss';
-                                                                                                                                                                                   
-const hi = greetings => {
-    const greetingNode = document.createElement('h1');
-    greetingNode.className = 'title';
-    greetingNode.innerText = greetings;
-
-    const imgNode = document.querySelector('img');
-    
-    if(imgNode){
-        imgNode.parentElement.insertBefore(greetingNode, imgNode);
-    }
-};
-
-const Hey = new hey('hi there');
-const greeting = Hey.getGreetings();
-hi(`${greeting} ${whatIsYourName()}`);
